bd484f4781f64610c904c051cb193dfc89905ed0
vrpdm1943q-lgtm <vrpdm1943q@gmail.com>
2025-08-31 18:55:08 +0530

    Create Entry.java

---
+++ app/java/com/yourpackage/ganeshexpenses/Entry.java
@@ -0,0 +1,17 @@
+public class Entry {
+    private String description;
+    private double amount;
+
+    public Entry(String description, double amount) {
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
