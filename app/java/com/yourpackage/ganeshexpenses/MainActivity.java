36dcd77535482e50116caca7e607cb02ba2d44ce
vrpdm1943q-lgtm <vrpdm1943q@gmail.com>
2025-08-31 18:56:28 +0530

    Create MainActivity.java

---
+++ app/java/com/yourpackage/ganeshexpenses/MainActivity.java
@@ -0,0 +1,27 @@
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String[] tabTitles = {"Income", "Expenses", "Summary"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Ganesh Expenses");
+        setContentView(R.layout.activity_main);
+
+        ViewPager2 viewPager = findViewById(R.id.viewPager);
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+
+        TabAdapter adapter = new TabAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager,
+                (tab, position) -> tab.setText(tabTitles[position])
+        ).attach();
+    }
+}
